4058250851d7c22d8432852358ccf2ed4187de47
Harry Keartland <harrykeartland@gmail.com>
2018-06-24 13:21:10 +0100

    added accuracy

--- main.js
+++ main.js
@@ -12,13 +12,16 @@ words.then(() => window.requestAnimationFrame(loop))
 function loop(){
   word = getRandomWord(words)
   document.getElementById("text").innerHTML ="<span>"+word+"</span>"
+  total = 0
   currentLetter = 0
   document.onkeydown = function(e) {
+    total++
     if (e.key.toLowerCase() == word[currentLetter]){
       currentLetter++
       document.getElementById("text").innerHTML = "<span style='color:#c4c2a9;text-shadow: .04em .04em 0 #809793;'>"+ word.substring(0,currentLetter) +"</span>" + "<span>"+word.substring(currentLetter,word.length)+"</span>"
     }
     if (currentLetter == word.length){
+      document.getElementById("acc").innerHTML = "Accuracy: " + Math.round(100*word.length/total,0) + "%"
       window.requestAnimationFrame(loop)
     }
   }
