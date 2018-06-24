cee23c9034c013ffdc75db4569f352ffc872f491
Harry Keartland <harrykeartland@gmail.com>
2018-06-24 13:10:53 +0100

    removed dealy which caused bugs

--- main.js
+++ main.js
@@ -13,13 +13,12 @@ function loop(){
   word = getRandomWord(words)
   document.getElementById("text").innerHTML ="<span>"+word+"</span>"
   currentLetter = 0
-  document.onkeydown = async function(e) {
+  document.onkeydown = function(e) {
     if (e.key.toLowerCase() == word[currentLetter]){
       currentLetter++
       document.getElementById("text").innerHTML = "<span style='color:#c4c2a9;text-shadow: .04em .04em 0 #809793;'>"+ word.substring(0,currentLetter) +"</span>" + "<span>"+word.substring(currentLetter,word.length)+"</span>"
     }
     if (currentLetter == word.length){
-      await new Promise(resolve => setTimeout(resolve, 500))
       window.requestAnimationFrame(loop)
     }
   }
